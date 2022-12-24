1f3262180bb28b2c9bf001e7784a81739727b652
Ibrahim M <89030061+IbrahimTurk@users.noreply.github.com>
2022-12-24 10:08:58 +0300

    update
    update

--- i_tech_product_memaar/models/product.py
+++ i_tech_product_memaar/models/product.py
@@ -6,17 +6,16 @@ from odoo import api, fields, models, _
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
+
     yarn_type = fields.Many2one('yarn.type', "Yarn Type")
     total_height = fields.Many2one('total.height', "Total Height")
     loop_weight = fields.Many2one('loop.weight', "Loop Weight")
 
+    yarn_type_setting =fields.Boolean(related="company_id.yarn_type")
+    total_height_setting =fields.Boolean(related="company_id.total_height")
+    loop_weight_setting =fields.Boolean(related="company_id.loop_weight")
 
 
-    #@api.model
-    def _aomunt(self):
-        company_id = self.env.company.id
-        print(company_id)
-        #return {'domain': {'journal_id': [('journal_user_id', '=', self.env.user.name)]}}    
 
 
 
